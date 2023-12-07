bb2b1bf56194d023fb92711fcc7e0a6be39fdb4e
Wang Qilin <qilin.wang@huawei.com>
2023-12-07 20:03:47 +0800

    add G.RS.10: check if license or license field exist

---
+++ src/rules/license_check.rs
@@ -0,0 +1,38 @@
+use crate::commands::Args;
+use crate::errors::{build_detail_err, BuildRule, CheckResult};
+use crate::rules::RuleChecker;
+use cargo_metadata::Metadata;
+
+#[derive(Debug, Clone, Default)]
+pub struct LicenseCheck;
+
+impl RuleChecker for LicenseCheck {
+    fn check(&self, m: &Metadata, _args: &Args) -> Vec<CheckResult<()>> {
+        let mut check_res = vec![];
+        for package in &m.packages {
+            if package.license.is_some() {
+                let err = build_detail_err(
+                    BuildRule::GRS10,
+                    "".to_string(),
+                    format!(
+                        "[G.RS.10] package `{}` contains `license` field",
+                        package.name
+                    ),
+                );
+                check_res.push(err)
+            }
+            if package.license_file.is_some() {
+                let err = build_detail_err(
+                    BuildRule::GRS10,
+                    "".to_string(),
+                    format!(
+                        "[G.RS.10] package `{}` contains `license-file` field",
+                        package.name
+                    ),
+                );
+                check_res.push(err)
+            }
+        }
+        check_res
+    }
+}
