c42070dbff40fc7a9e388a3def417d59480693a2
w00870265 <wangluxing3@huawei.com>
2024-01-04 11:35:38 +0800

    修改构建流程 新增打包过程

--- .cloudbuild/build.sh
+++ .cloudbuild/build.sh
@@ -1,2 +1,14 @@
 # Run `cargo build --release`
 cargo build --release
+
+# Package 
+if [-z "${version}"]; then
+  echo "The version environment variable is not set."
+  exit 1
+fi
+
+ARCH=$(uname -m)
+FILENAME="build-inspector-${version}-${ARCH}.tar.gz"
+
+cd target/release
+tar -czvf "${FILENAME}" build-inspector
